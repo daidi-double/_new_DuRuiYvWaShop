ec44b56ba545a6c2ec58b0a92fb39cd114871334
Your Name <934940566@qq.com>
2017-04-14 10:25:39 +0800

    无

--- DuRuiYvWaShop/YuWaShop/YuWaShop/Home/SubVC/AdvanceOrder/YWHomeAdvanceOrderViewController.h
+++ DuRuiYvWaShop/YuWaShop/YuWaShop/Home/SubVC/AdvanceOrder/YWHomeAdvanceOrderViewController.h
@@ -3,7 +3,7 @@
 //  YuWaShop
 //
 //  Created by Tian Wei You on 16/12/1.
-//  Copyright © 2016年 Shanghai DuRui Information Technology Company. All rights reserved.
+//  Copyright ©   201 6年 Shanghai DuRui Information Technology Company. All rights reserved.
 //
 
 #import "JWBasicViewController.h"
