0da45b893846032c793c0fc30aadb9fc5196146b
double <double@yuwabao.cn>
2017-04-17 17:50:21 +0800

    空格

--- DuRuiYvWaShop/YuWaShop/YuWaShop/Person/YWPersonViewController.m
+++ DuRuiYvWaShop/YuWaShop/YuWaShop/Person/YWPersonViewController.m
@@ -55,6 +55,7 @@
 - (void)makeUI{
     WEAKSELF;
     self.headerView = [[[NSBundle mainBundle]loadNibNamed:@"YWPersonHeaderView" owner:nil options:nil]firstObject];
+    
     self.headerView.chooseBtnBlock = ^(NSInteger choosedBtn){//1门店2会员3分红
         UIViewController * vc;
         switch (choosedBtn) {
