f0e47bfc2ec4e5de50cf1b57c1a8bd0795a04470
Your Name <934940566@qq.com>
2017-04-25 14:11:07 +0800

    无

--- DuRuiYvWaShop/YuWaShop/YuWaShop/AppDelegate.m
+++ DuRuiYvWaShop/YuWaShop/YuWaShop/AppDelegate.m
@@ -3,7 +3,7 @@
 //  YuWaShop
 //
 //  Created by Tian Wei You on 16/11/17.
-//  Copyright © 2016年 Shanghai DuRui Information Technology Company. All rights reserved.
+//  Copyright © 2016年 Shanghai DuRui Information Technology Company. All rights reserved. 
 //
 
 #import "AppDelegate.h"
