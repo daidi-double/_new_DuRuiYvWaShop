f9db0cdc002eb829b1da42c57ddc2f70488de212
Your Name <934940566@qq.com>
2017-04-26 15:05:09 +0800

    侧滑效果实现（完成）

--- DuRuiYvWaShop/YuWaShop/YuWaShop/mainController/VIPNavigationController.m
+++ DuRuiYvWaShop/YuWaShop/YuWaShop/mainController/VIPNavigationController.m
@@ -15,27 +15,39 @@
     [self.navigationBar setTintColor:[UIColor whiteColor]];
     [self.navigationBar setBarTintColor:CNaviColor];
     [self.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
-    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+    {
+        self.interactivePopGestureRecognizer.enabled = YES;      // 手势有效设置为YES  无效为NO
+        self.interactivePopGestureRecognizer.delegate = self;    // 手势的代理设置为self
+    }
+
 }
 
 -(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
-//    if (self.viewControllers.count>0) {
-//        viewController.hidesBottomBarWhenPushed=YES;
-//        
-//        UIButton*button=[[UIButton alloc]init];
-//        [button setBackgroundImage:[UIImage imageNamed:@"NaviBack"] forState:UIControlStateNormal];
-//        [button setBackgroundImage:[UIImage imageNamed:@"NaviBack"] forState:UIControlStateHighlighted];
-//        CGSize buttonSize =button.currentBackgroundImage.size;
-//        button.frame=CGRectMake(button.frame.origin.x, button.frame.origin.y, buttonSize.width, buttonSize.height);
-//        [button addTarget:self action:@selector(buttonBack) forControlEvents:UIControlEventTouchUpInside];
-//        UIBarButtonItem*leftItem=[[UIBarButtonItem alloc]initWithCustomView:button];
-//        viewController.navigationItem.leftBarButtonItem=leftItem;
-////        viewController.navigationItem.backBarButtonItem=leftItem;
-//        
-//}
+    if (self.viewControllers.count>0) {
+        viewController.hidesBottomBarWhenPushed=YES;
+        
+        UIButton*button=[[UIButton alloc]init];
+        [button setBackgroundImage:[UIImage imageNamed:@"NaviBack"] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:@"NaviBack"] forState:UIControlStateHighlighted];
+        CGSize buttonSize =button.currentBackgroundImage.size;
+        button.frame=CGRectMake(button.frame.origin.x, button.frame.origin.y, buttonSize.width, buttonSize.height);
+        [button addTarget:self action:@selector(buttonBack) forControlEvents:UIControlEventTouchUpInside];
+        UIBarButtonItem*leftItem=[[UIBarButtonItem alloc]initWithCustomView:button];
+        viewController.navigationItem.leftBarButtonItem=leftItem;
+//        viewController.navigationItem.backBarButtonItem=leftItem;
+        
+}
     [super pushViewController:viewController animated:animated];
 }
-
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    if (self.navigationController.viewControllers.count == 1) {
+        return NO;
+    }else{
+        return YES;
+    }
+}
 -(void)buttonBack{
     [self popViewControllerAnimated:YES];
     
